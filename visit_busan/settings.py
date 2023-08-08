da23b4f82fb3e7912c29d4c2bd6621b28cc702f4
chea-young <lchy0413@gmail.com>
2023-08-08 23:34:10 +0900

    Feat: add ec2 ip in setting

--- visit_busan/settings.py
+++ visit_busan/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('EC2_IP')]
 
 # Application definition
 INSTALLED_APPS = [
