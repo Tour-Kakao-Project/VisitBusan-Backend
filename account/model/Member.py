e4335310ae8ca88f2f6e9cdb2c7160a22895410f
chea-young <lchy0413@gmail.com>
2023-08-15 22:48:45 +0900

    Feat: add Member table

--- account/model/Member.py
+++ account/model/Member.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+OAUTH_PROVIDER_CHOICES = [
+    ('1', 'NONE'),
+    ('2', 'KAKAO'),
+    ('3', 'GOOGLE'),
+]
+
 class Member(models.Model):
-    #user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.CharField(max_length=50) # 이메일
-    first_name = models.CharField(max_length=50) # 이름: 이름
-    last_name = models.CharField(max_length=50) # 이름: 성
+    email = models.EmailField(max_length=300) # 이메일
+    passwd = models.CharField( max_length=50, blank=True)
+    first_name = models.CharField(max_length=30, blank=True) # 이름: 이름
+    last_name = models.CharField(max_length=150, blank=True) # 이름: 성
+    phone_number = models.CharField(max_length=15, blank=True)
+    
+    oauth_provider = models.CharField(max_length=50, choices=OAUTH_PROVIDER_CHOICES, default=OAUTH_PROVIDER_CHOICES[0]) # 로그인 제공
 
     def __str__(self):
         return self.email  # 닉네임 값을 대표값으로 설정
--- visit_busan/settings.py
+++ visit_busan/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [env('EC2_IP')]
+ALLOWED_HOSTS = [env('EC2_IP'), 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
