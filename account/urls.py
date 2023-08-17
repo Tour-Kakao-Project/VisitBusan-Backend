f7f89eebbb4da21b863ecb82a70aeeb2ffa47985
chea-young <lchy0413@gmail.com>
2023-08-17 23:31:09 +0900

    Feat: add kakao login feature

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # local test
+    ## kakao login
+    path('back/oauth/kakao', views.KakaoLogin.kakao_back_login),
+    path('oauth/kakao', views.KakaoLogin.kakao_back_login_redirect),
+    
+    path('login/oauth/kakao', views.KakaoLogin.kakao_login),
 ]
