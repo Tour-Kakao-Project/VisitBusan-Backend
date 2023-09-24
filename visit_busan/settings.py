2a0d4cdacd4f3789baae480e60c63dc4c589361f
chea-young <63410509+chea-young@users.noreply.github.com>
2023-09-24 14:49:14 +0900

    Feat: add android host

--- visit_busan/settings.py
+++ visit_busan/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [env("EC2_IP"), "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [env("EC2_IP"), "localhost", "127.0.0.1", "api.ipify.org"]
 
 # Application definition
 INSTALLED_APPS = [
