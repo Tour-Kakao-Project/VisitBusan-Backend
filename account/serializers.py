d7d519471d3045ad374dedd98f9549d9d33395c3
chea-young <lchy0413@gmail.com>
2023-10-10 22:20:47 +0900

    Feat: add 'user info'

--- account/serializers.py
+++ account/serializers.py
@@ -4,6 +4,11 @@ from .model.index import *
 
 
 class MemberSerializers(serializers.ModelSerializer):
+    oauth_provider = serializers.SerializerMethodField()
+
+    def get_oauth_provider(self, obj):
+        return obj.get_oauth_provider_display()
+
     class Meta:
         model = Member
         fields = [
